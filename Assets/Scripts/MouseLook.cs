028bb36c161bbb2b0650bf809ed397b06f0fd9f4
joshsj89 <joshsj89@gmail.com>
2024-02-13 00:56:55 -0800

    Add Camera/Hover Lock
    -Cursor visibility stays on
    -Changed input key to "Space"

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,23 +9,34 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
 
     float xRotation = 0f; // The rotation of the camera along the x-axis (up and down)
+
+    bool locked = false; // The camera rotation lock
     
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; // Locks the cursor to the center of the screen
+        // Cursor.lockState = CursorLockMode.Locked; // Locks the cursor to the center of the screen
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // mouseX is the horizontal movement of the mouse
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // mouseY is the vertical movement of the mouse
+        if (!locked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; // mouseX is the horizontal movement of the mouse
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; // mouseY is the vertical movement of the mouse
 
-        xRotation -= mouseY; // invert the mouseY to make the camera move in the correct direction
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Prevents the player from looking behind them
+            xRotation -= mouseY; // invert the mouseY to make the camera move in the correct direction
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Prevents the player from looking behind them
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Rotates the camera up and down
-        playerBody.Rotate(Vector3.up * mouseX); // Rotates the player left and right
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); // Rotates the camera up and down
+            playerBody.Rotate(Vector3.up * mouseX); // Rotates the player left and right
+        }
+    }
+
+    public bool Locked
+    {
+        get { return locked; }
+        set { locked = value; }
     }
 }
--- Assets/Scripts/Reticle.cs
+++ Assets/Scripts/Reticle.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Reticle : MonoBehaviour
 {
+    MouseLook mouseLook;
+
+    bool visible = false;
+    
     private void Start() {
-        Cursor.visible = false;
+        // Cursor.visible = visible;
+
+        mouseLook = FindAnyObjectByType<MouseLook>();
     }
 
     private void Update()
     {
         //if escape
         //cursor.visible = true
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            Cursor.visible = true;
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            visible = !visible;
+            // Cursor.visible = visible;
+            mouseLook.Locked = visible;
         }
 
         transform.position = Input.mousePosition;
