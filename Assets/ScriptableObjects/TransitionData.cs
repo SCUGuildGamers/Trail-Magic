e0026e1d7ea14da9cee5a240bc22d88e4cd4a9cc
SCU Imaginarium (Samus) <afra.mahammad@gmail.com>
2024-03-14 17:33:21 -0700

    transition update

--- Assets/ScriptableObjects/TransitionData.cs
+++ Assets/ScriptableObjects/TransitionData.cs
@@ -6,8 +6,55 @@ using UnityEngine;
 
 public class TransitionData : ScriptableObject
 {
-    public Gradient sunColor;
+
+    //skybox data
+    public Gradient skyBoxIntensity;
     public Gradient skyTopColor;
     public Gradient skyBottomColor;
-    public Gradient skyExponent;
+    public Gradient skyGradExponent;
+    public Gradient sunColor;
+
+    //fog settings
+    public Gradient fogColor;
+    public Gradient fogDensity;
+    public Gradient envLightingIntensityMult;
+
+
+    //directional light data
+    public Gradient emissionColor;
+    public Gradient emissionIntensity;
+
+
+    //bloom
+    public Gradient bloomThreshhold;
+    public Gradient bloomIntensity;
+    public Gradient bloomScatter;
+    public Gradient bloomTint;
+
+
+    //vignette
+    public Gradient vignetteColor;
+    public Gradient vignetteIntensity;
+    public Gradient vignetteSmoothness;
+
+    //color adjustments
+    public Gradient adjustPostExposure;
+    public Gradient adjustContrast;
+    public Gradient adjustColorFilter;
+    public Gradient adjustHueShift;
+    public Gradient adjustSaturation;
+
+    //shadows midtones highlights
+    public Gradient smhShadows;
+    public Gradient smhMidtones;
+    public Gradient smhHighlights;
+
+    public Gradient smhShadowStart;
+    public Gradient smhShadowEnd;
+
+    public Gradient smhHighlightStart;
+    public Gradient smhHighlightEnd;
+
+
+
 }
