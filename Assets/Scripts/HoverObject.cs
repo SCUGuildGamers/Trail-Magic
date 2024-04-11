c54a184f6e0e51bdc2117a6080d0dbb9a9d57bc2
alexlucaslopes <alex.lucas.lopes@gmail.com>
2024-04-11 15:26:02 -0700

    Text Pop-Up with Outline

--- Assets/Scripts/HoverObject.cs
+++ Assets/Scripts/HoverObject.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class HoverObject : MonoBehaviour
 {
+    [SerializeField] private GameObject floatingTextPrefab;
+    
+    private GameObject prefab;
     void Start(){
+        prefab = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity);
         Unhover();
     }
     public void Hover() {
         Debug.Log("Hovering!");
         if(GetComponent<Outline>() != null){
             GetComponent<Outline>().enabled = true;
+            prefab.SetActive(true);
         }
     }
 
     public void Unhover()
     {
         Debug.Log("Not hovering!");
-        if(GetComponent<Outline>() != null)
+        if(GetComponent<Outline>() != null){
             GetComponent<Outline>().enabled = false;
+            prefab.SetActive(false);
+        }
     }
+
 }
