4caa4df7553a7d2fc4eeea21a34b7e64d2ef2ab8
alexlucaslopes <alex.lucas.lopes@gmail.com>
2024-04-17 14:12:13 -0700

    Basic Polaroid Functionality

---
+++ Assets/Scripts/RenderTextureAlign.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTextureAlignScript : MonoBehaviour
+{
+    [SerializeField] Camera thisCam;
+    [SerializeField] Camera mainCam;
+    [SerializeField] Material thisCamMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCam = GetComponent<Camera>();
+        mainCam = Camera.main;
+        if (thisCam.targetTexture != null) {
+            thisCam.targetTexture.Release();
+        }
+
+        if (mainCam.targetTexture != null) {
+            mainCam.targetTexture.Release();
+        }
+
+        thisCam.targetTexture.width = Screen.width;
+        thisCam.targetTexture.height = Screen.height;
+
+        thisCamMaterial.mainTexture = thisCam.targetTexture;
+    }
+}
