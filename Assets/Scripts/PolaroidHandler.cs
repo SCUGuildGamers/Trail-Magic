1fd98d9ee8a188393fa0a3dfdc23bb80744252fb
alexlucaslopes <alex.lucas.lopes@gmail.com>
2024-04-24 23:25:23 -0700

    More Complete Polaroid Handler
    Current issue is just getting the key input to actually work

--- Assets/Scripts/PolaroidHandler.cs
+++ Assets/Scripts/PolaroidHandler.cs
@@ -3,23 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PolaroidHandler : MonoBehaviour
-{
-    GameObject polaroidCam = GameObject.FindGameObjectWithTag("PolaroidCamera");
-    
-    private void Update() 
-    {
-        /*
-        if () //if Player is in specific Collider
-        {
-            //Display UI element that tells the player to press E
+{   [SerializeField] string polaroidTag;
+    [SerializeField] GameObject polaroidCam;
 
-            if (Input.GetKeyDown(KeyCode.E)) {
+    [SerializeField] private GameObject floatingTextPrefab;
+    private GameObject uiElement;
+
+    private bool polaroidState;
+    
+    void Start() {
+        polaroidState = false;
+        uiElement = Instantiate(floatingTextPrefab, transform.position, Quaternion.identity);
+        PolaroidStateHandler();
+    }
+    
+    void Update() {
+        PolaroidStateHandler();
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        if(collider.gameObject.tag == polaroidTag) {
+            Debug.Log("Triggered with: " + collider.gameObject.name);
+            polaroidState = true; 
+        }
+    }
+
+    void OnTriggerExit(Collider collider) {
+        if(collider.gameObject.tag == polaroidTag) {
+            Debug.Log("Triggered with: " + collider.gameObject.name);
+            polaroidState = false;
+        }
+    }
+
+    void PolaroidStateHandler () {
+        if (polaroidState == true) {
+            uiElement.SetActive(true);
+            if (Input.GetKey(KeyCode.Space)) {
+                Debug.Log("space");
                 polaroidCam.SetActive(true);
+                uiElement.SetActive(false);
             }
         }
         else {
             polaroidCam.SetActive(false);
-        }
-        */
+            uiElement.SetActive(false);
+        }    
     }
 }
