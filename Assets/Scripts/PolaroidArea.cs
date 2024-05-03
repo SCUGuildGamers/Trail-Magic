5787ea24e6a14118fa78c62194418a5aa95120d2
alexlucaslopes <alex.lucas.lopes@gmail.com>
2024-05-03 12:55:24 -0700

    I GOT THE POLAROIDS TO WORK (Done!)

---
+++ Assets/Scripts/PolaroidArea.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolaroidArea : MonoBehaviour
+{
+    [SerializeField] string timeframe;
+    
+    public string getTimeframe(){
+        return timeframe;
+    }
+}
--- Assets/Scripts/PolaroidHandler.cs
+++ Assets/Scripts/PolaroidHandler.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PolaroidHandler : MonoBehaviour
 {   [SerializeField] string polaroidTag;
     [SerializeField] GameObject polaroidCam;
+    [SerializeField] GameObject eightiesPolaroid;
+    [SerializeField] GameObject ninetiesPolaroid;
+    [SerializeField] GameObject bwPolaroid;
 
     [SerializeField] private GameObject floatingTextPrefab;
     private GameObject uiElement;
-
     private bool polaroidState;
     
     void Start() {
@@ -23,14 +25,29 @@ public class PolaroidHandler : MonoBehaviour
 
     void OnTriggerEnter(Collider collider) {
         if(collider.gameObject.tag == polaroidTag) {
-            Debug.Log("Triggered with: " + collider.gameObject.name);
+            Debug.Log("Entering!");
             polaroidState = true; 
+            string era = collider.gameObject.GetComponent<PolaroidArea>().getTimeframe();
+            if(era == "bw") {
+                eightiesPolaroid.SetActive(true);
+            }
+            else if (era == "80s") {
+                ninetiesPolaroid.SetActive(true);
+            }
+            else if (era == "90s") {
+                bwPolaroid.SetActive(true);
+            }  
+            else {
+                eightiesPolaroid.SetActive(false);
+                ninetiesPolaroid.SetActive(false);
+                bwPolaroid.SetActive(false);
+            }
         }
     }
 
     void OnTriggerExit(Collider collider) {
         if(collider.gameObject.tag == polaroidTag) {
-            Debug.Log("Triggered with: " + collider.gameObject.name);
+            Debug.Log("Exiting!");
             polaroidState = false;
         }
     }
@@ -38,9 +55,9 @@ public class PolaroidHandler : MonoBehaviour
     void PolaroidStateHandler () {
         if (polaroidState == true) {
             uiElement.SetActive(true);
-            if (Input.GetKey(KeyCode.Space)) {
-                Debug.Log("space");
-                polaroidCam.SetActive(true);
+            if (Input.GetKey("space")) {
+                Debug.Log("Space was pressed");
+                polaroidCam.SetActive(true); 
                 uiElement.SetActive(false);
             }
         }
