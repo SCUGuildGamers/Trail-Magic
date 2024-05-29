3992d0a18882b0ef7c5ac06ae933ea6e36bd165b
joshsj89 <joshsj89@gmail.com>
2024-05-29 13:38:53 -0700

    Add volume slider

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] FullscreenToggle fullscreenToggle;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] TextMeshProUGUI volumeTextUI;
     
     // Start is called before the first frame update
     void Start()
     {
-        if(!PlayerPrefs.HasKey("fullscreenValue"))
+        if (!PlayerPrefs.HasKey("gameVolume") && !PlayerPrefs.HasKey("fullscreenValue"))
+        {
+            PlayerPrefs.SetFloat("gameVolume", 1);
+            PlayerPrefs.SetInt("fullscreenValue", 0);
+            Load();
+        }
+        
+        else if (!PlayerPrefs.HasKey("gameVolume"))
+        {
+            PlayerPrefs.SetFloat("gameVolume", 1);
+            Load();
+        }
+        else if(!PlayerPrefs.HasKey("fullscreenValue"))
         {
             PlayerPrefs.SetInt("fullscreenValue", 0);
             fullscreenToggle.isOn = false;
@@ -24,8 +40,19 @@ public class OptionsMenu : MonoBehaviour
 
     private void Load()
     {
+        volumeSlider.value = PlayerPrefs.GetFloat("gameVolume");
+        AudioListener.volume = volumeSlider.value;
+
         fullscreenToggle.isOn = PlayerPrefs.GetInt("fullscreenValue") > 0;
         Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextUI.text = volume.ToString("0%");
+        PlayerPrefs.SetFloat("gameVolume", volume);
+
     }
 
     public void SetFullscreen(bool isFullscreen)
